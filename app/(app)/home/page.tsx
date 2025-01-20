a6bf917da7552ad3d406165d0017b074ae98524b
Pedro Baptista <pedrolk2012@gmail.com>
2025-01-20 13:03:57 -0300

    test: create products fetcher

--- app/(app)/home/page.tsx
+++ app/(app)/home/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { useFetchProducts } from "@/modules/products/hooks/use-fetch-products";
+
 export default function Page() {
+  const { data } = useFetchProducts();
   return (
-    <div>
+    <div className="flex flex-col gap-4">
       <h1 className="text-6xl">Home</h1>
+      {data?.data.map((item) => {
+        return <div key={item.id}>{item.name} </div>;
+      })}
     </div>
   );
 }
