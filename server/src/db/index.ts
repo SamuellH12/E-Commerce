0fe04a865c125a96d60b31908e5e18d0597739da
Pedro Baptista <pedrolk2012@gmail.com>
2025-02-09 10:06:34 -0300

    feature: change process env names to more descriptive ones

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,15 +1,15 @@
-import { createClient } from '@supabase/supabase-js'
-import dotenv from 'dotenv'
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const supabaseUrl = process.env.URL_PROJECT || ''
-const supabaseKey = process.env.API_KEY || ''
+const supabaseUrl = process.env.SUPABASE_URL || "";
+const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
 
 if (!supabaseUrl || !supabaseKey) {
-  throw new Error('Missing Supabase URL or API key')
+  throw new Error("Missing Supabase URL or API key");
 }
 
-const supabase = createClient(supabaseUrl, supabaseKey)
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
+export default supabase;
