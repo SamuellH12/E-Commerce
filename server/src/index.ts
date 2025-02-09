18b35143decf41ab3f9f13d13869091f89d33fd2
LucasSilvaa0 <lucaspernambucano@live.com>
2025-02-09 13:44:18 -0300

    Fix: adapting code to run

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,5 +15,5 @@ app.use("/cards", cors(), cardRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+	console.log(`Server is running on port ${PORT}`);
 });
