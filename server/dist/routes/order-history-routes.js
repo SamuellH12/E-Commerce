5e8f89bb3698a04d0ef2df4ceb2512cc430718ea
Pedro Baptista <pedrolk2012@gmail.com>
2025-02-10 08:35:48 -0300

    build: remove unused dist folder

--- server/dist/routes/order-history-routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.orderHistoryRouter = void 0;
-const express_1 = require("express");
-const orderHistory_controllers_1 = require("../controllers/orderHistory/orderHistory-controllers");
-const orderHistoryRouter = (0, express_1.Router)();
-exports.orderHistoryRouter = orderHistoryRouter;
-// Define a rota GET para buscar o histórico de pedidos
-orderHistoryRouter.get("/", orderHistory_controllers_1.getOrderHistory);
--- server/dist/routes/productOrderHistory-router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.productOrderHistoryRouter = void 0;
-const express_1 = require("express");
-const productOrderHistory_controllers_1 = require("../controllers/productOrderHistory/productOrderHistory-controllers");
-const productOrderHistoryRouter = (0, express_1.Router)();
-exports.productOrderHistoryRouter = productOrderHistoryRouter;
-// Define a rota GET para buscar os itens de um pedido
-productOrderHistoryRouter.get("/", productOrderHistory_controllers_1.getProductOrderHistory);
