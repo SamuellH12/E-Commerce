726e905742ccc25ae32483f066cf1d7bd657e8c6
Pedro Baptista <pedrolk2012@gmail.com>
2025-02-11 18:28:03 -0300

    feat(ProductControllers): add update, create, delete and get product functions

--- server/src/controllers/product/product-controllers.ts
+++ server/src/controllers/product/product-controllers.ts
@@ -9,4 +9,52 @@ export async function getAllProducts(req: Request, res: Response) {
   res.json(data);
 }
 
-// todo: integrar com BD
+export async function updateProduct(req: Request, res: Response) {
+  const id: string = req.body.id;
+
+  if (!id) res.status(400).send("id is required!");
+
+  const { error } = await supabase
+    .from("products")
+    .update(req.body)
+    .eq("id", id);
+
+  if (error) res.status(400).json(error);
+
+  res.send("Product updated successfully");
+}
+
+export async function createProduct(req: Request, res: Response) {
+  const { error } = await supabase.from("products").insert(req.body);
+
+  if (error) res.status(400).json(error);
+
+  res.send("Product created successfully");
+}
+
+export async function deleteProduct(req: Request, res: Response) {
+  const id: string = req.body.id;
+
+  if (!id) res.status(400).send("id is required!");
+
+  const { error } = await supabase
+    .from("products")
+    .delete()
+    .eq("some_column", id);
+
+  if (error) res.status(400).json(error);
+
+  res.send("Product deleted successfully");
+}
+
+export async function getProduct(req: Request, res: Response) {
+  const id: string = req.body.id;
+
+  if (!id) res.status(400).send("id is required!");
+
+  const { error, data } = await supabase.from("products").select().eq("id", id);
+
+  if (error) res.status(400).json(error);
+
+  res.json(data?.[0] ?? null);
+}
