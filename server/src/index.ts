6193c1df4d06ae0088315ef0459c86387d64ff1a
SamuellH12 <84053584+SamuellH12@users.noreply.github.com>
2025-02-12 18:09:31 -0300

    Departamentos adicionados

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,8 @@ import express from "express";
 import { cardRouter } from "./routes/card-routes";
 import { couponsRouter } from "./routes/coupons-routes";
 import { productRouter } from "./routes/product-routes";
+import { categoriesRouter } from "./routes/categories-routes";
+import { departmentRouter } from "./routes/departament-routes";
 import { orderHistoryRouter } from "./routes/order-history-routes";
 import { productOrderHistoryRouter } from "./routes/product-order-history-routes";
 
@@ -17,6 +19,8 @@ app.use(cors());
 app.use("/products", cors(), productRouter);
 app.use("/cards", cors(), cardRouter);
 app.use("/coupons", cors(), couponsRouter);
+app.use("/category", cors(), categoriesRouter);
+app.use("/department", cors(), departmentRouter);
 app.use("/order-history", orderHistoryRouter);
 app.use("/product-order-history", productOrderHistoryRouter);
 
