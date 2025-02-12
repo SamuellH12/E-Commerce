606af4d10f17e26c30832b64825efaa423d4b077
Pedro Baptista <pedrolk2012@gmail.com>
2025-02-12 12:32:26 -0300

    feature: remove try catch from bdd tests

--- server/features/steps_definitions/steps_defs.mjs
+++ server/features/steps_definitions/steps_defs.mjs
@@ -4,13 +4,14 @@ import { expect } from "chai";
 
 const BASE_URL = process.env.BASE_URL;
 
-Given("o usuário está na página de {string}", (string) => {
+Given("o usuário está na página de {string}", function (string) {
   // This isn't necessary to service scenario
 });
 
 Given(
   "o usuário tem os cartões de apelido {string} e {string} cadastrados",
-  (string, string2) => {
+  function (string, string2) {
+    this.cardId = "3123";
     // Write code here that turns the phrase above into concrete actions
     return "pending";
   }
@@ -18,7 +19,7 @@ Given(
 
 When(
   "o usuário seleciona a opção {string} o cartão {string}",
-  (string, string2) => {
+  function (string, string2) {
     // Write code here that turns the phrase above into concrete actions
     return "pending";
   }
@@ -32,66 +33,53 @@ Then(
   }
 );
 
-Then("o usuário tem os cartões de apelido {string} cadastrados", (string) => {
-  // Write code here that turns the phrase above into concrete actions
-  return "pending";
-});
-
-When(
-  "o usuário preenche o apelido {string}, o nome {string}, o código {string}, o vencimento {string} e o cvc {string}",
-  async (string1, string2, string3, string4, string5) => {
-    try {
-      // Debug para verificar os dados enviados
-      const response = await axios.post("http://localhost:3000/cards/new", {
-        nickname: string1,
-        name: string2,
-        code: string3,
-        expiration: string4,
-        cvc: string5,
-      });
-
-      expect(response.status).to.equal(200);
-    } catch (error) {
-      console.error(
-        "Erro na requisição:",
-        error.response?.status,
-        error.response?.data || error.message
-      );
-      throw new Error("Falha ao cadastrar o cartão");
-    }
+Then(
+  "o usuário tem os cartões de apelido {string} cadastrados",
+  function (string) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
   }
 );
 
-When("o usuário seleciona a opção {string} com sucesso", (string) => {
+When(
+  "o usuário preenche o apelido {string}, o nome {string}, o código {string}, o vencimento {string} e o cvc {string}",
+  async function (string1, string2, string3, string4, string5) {
+    // Debug para verificar os dados enviados
+    const response = await axios.post("http://localhost:3000/cards/new", {
+      nickname: string1,
+      name: string2,
+      code: string3,
+      expiration: string4,
+      cvc: string5,
+    });
+
+    expect(response.status).to.equal(200);
+  }
+);
+
+When("o usuário seleciona a opção {string} com sucesso", function (string) {
   // Write code here that turns the phrase above into concrete actions
+
   return "pending";
 });
 
 Then(
   "o cartão é salvo na conta com o apelido {string}, o nome {string}, os quatro últimos dígitos {string} e o tipo {string}",
-  async (string, string2, string3, string4) => {
+  async function (string, string2, string3, string4) {
     // Write code here that turns the phrase above into concrete actions
-    try {
-      await axios.get("http://localhost:3000/cards").then((response) => {
-        const cards = response.data;
-        const card = cards.find((card) => card.nickname === string);
 
-        expect(card.name).to.equal(string2);
-        expect(card.code_last4).to.equal(string3);
-        expect(card.card_type).to.equal(string4);
-      });
-    } catch (error) {
-      console.error(
-        "Erro na requisição:",
-        error.response?.status,
-        error.response?.data || error.message
-      );
-      throw new Error("Falha ao verificar o cartão");
-    }
+    await axios.get("http://localhost:3000/cards").then((response) => {
+      const cards = response.data;
+      const card = cards.find((card) => card.nickname === string);
+
+      expect(card.name).to.equal(string2);
+      expect(card.code_last4).to.equal(string3);
+      expect(card.card_type).to.equal(string4);
+    });
   }
 );
 
-Then("o cartão é validado", () => {
+Then("o cartão é validado", function () {
   // Write code here that turns the phrase above into concrete actions
   return "pending";
 });
@@ -122,20 +110,16 @@ When(
   async function (name, description, price, category, stock_quantity, img_url) {
     // Write code here that turns the phrase above into concrete actions
 
-    try {
-      const response = await axios.post(`http://localhost:3000/products/new`, {
-        name: name,
-        price: +price, // Fix: price should be assigned correctly
-        stock_quantity: Number(stock_quantity),
-        is_active: true,
-        description: description,
-        image_url: img_url,
-      });
-      this.productId = response.data.id;
-      this.productCreateData = response.data;
-    } catch (error) {
-      throw new Error(error);
-    }
+    const response = await axios.post(`http://localhost:3000/products/new`, {
+      name: name,
+      price: +price, // Fix: price should be assigned correctly
+      stock_quantity: Number(stock_quantity),
+      is_active: true,
+      description: description,
+      image_url: img_url,
+    });
+    this.productId = response.data.id;
+    this.productCreateData = response.data;
 
     // Store the product ID in the context (this)
   }
@@ -144,35 +128,23 @@ When(
 Then("o sistema deve validar os dados inseridos", async function () {
   // Write code here that turns the phrase above into concrete actions
 
-  try {
-    const response = await axios.get(
-      `http://localhost:3000/products/${this.productId}`
-    );
+  const response = await axios.get(
+    `http://localhost:3000/products/${this.productId}`
+  );
 
-    console.log("");
-
-    if (
-      JSON.stringify(response.data) !== JSON.stringify(this.productCreateData)
-    ) {
-      throw new Error("Dados estão inválidos");
-    }
-  } catch (error) {
-    return error;
-  }
+  expect(JSON.stringify(response.data)).to.be.eql(
+    JSON.stringify(this.productCreateData)
+  );
 });
 
 Then(
   "o item deve ser exibido na lista de itens cadastrados",
   async function () {
     // Write code here that turns the phrase above into concrete actions
-    try {
-      const response = await axios.get(`http://localhost:3000/products`);
-      const idx = !!response.data.findIndex(
-        (item) => item.id === this.productId
-      );
-      if (idx === -1) throw new Error("Item não encontrado na lista");
-    } catch (error) {
-      throw new Error(error);
-    }
+
+    const response = await axios.get(`http://localhost:3000/products`);
+
+    const idx = response.data.findIndex((item) => item.id === this.productId);
+    expect(idx).to.be.greaterThan(-1);
   }
 );
