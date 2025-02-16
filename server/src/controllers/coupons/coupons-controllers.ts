72971c05cf9331a96be208a6fe1ccf8d48d96abb
Lucas Guimarães <cs.lucas.guimaraes@gmail.com>
2025-02-16 13:31:10 -0300

    feat: update coupons

--- server/src/controllers/coupons/coupons-controllers.ts
+++ server/src/controllers/coupons/coupons-controllers.ts
@@ -12,13 +12,21 @@ export async function addCoupon(req: Request, res: Response) {
 }
 
 export async function deleteCoupon(req: Request, res: Response) {    
-  const { error } = await supabase
-  .from('coupons')
-  .delete()
-  .eq('id', +req.params.couponID)
-  
-  if (error) res.status(500).json(error);
-  else res.json("DELETED ID " + req.params.couponID);
+  if (isNaN(+req.params.couponID)) {
+    const {error} = await supabase
+    .from('coupons')
+    .delete()
+    .eq('codename', req.params.couponID)
+    if (error) res.status(500).json(error);
+    else res.json("DELETED NAME " + req.params.couponID);
+  } else {
+    const { error } = await supabase
+    .from('coupons')
+    .delete()
+    .eq('id', +req.params.couponID)
+    if (error) res.status(500).json(error);
+    else res.json("DELETED ID " + req.params.couponID);
+  }
 }
 
 export async function readAllCoupons(req: Request, res: Response) {  
@@ -27,6 +35,28 @@ export async function readAllCoupons(req: Request, res: Response) {
   .from('coupons')
   .select('*')
   
+  if (error) res.status(500).json(error);
+  else res.json(data);
+}
+
+export async function updateCoupons(req: Request, res: Response) {
+  const { data: dd, error: ee } = await supabase
+  .from('coupons')
+  .select('codename')
+  .eq('codename', req.body.name)
+
+  if (!dd || dd.length === 0) {
+    res.status(404).json({ error: "Coupon not found" });
+    return;
+  }
+  if (ee) res.status(500).json(ee);
+
+  const { data, error } = await supabase
+  .from('coupons')
+  .update({ percentage: req.body.percentage, expiration_date: req.body.expiration_date || null })
+  .eq('codename', req.body.name)
+  .select()
+
   if (error) res.status(500).json(error);
   else res.json(data);
 }
