9992a57747bcff4eed619e2215c9861a22debe41
LucasSilvaa0 <lucaspernambucano@live.com>
2025-02-24 15:59:41 -0300

    feat(paymeny/page): create remove and update buttons

--- client/app/(app)/payment/page.tsx
+++ client/app/(app)/payment/page.tsx
@@ -24,6 +24,7 @@ export default function Payment() {
     const [optionSelected, setOptionSelected] = useState(0);
     const [cardSelected, setCardSelected] = useState(0);
     const [cards, setCards] = useState<Card[]>([]);
+    const [menuOpen, setMenuOpen] = useState(0);
     
     useEffect(() => {
         const fetchData = async () => {
@@ -34,6 +35,27 @@ export default function Payment() {
     
         fetchData();
     }, []);
+
+    function atualizarMenu(id:number) {
+        if (menuOpen === id) {
+            setMenuOpen(0);
+        } else {
+            setMenuOpen(id);
+        }
+    }
+
+    async function confirmarRemocao(id:number) {
+        const answer = window.prompt("TEM CERTEZA QUE DESEJA REMOVER ESSE CARTÃO? (S/N)")?.toUpperCase()
+
+        if (answer === "S") {
+            const response = await fetch(`http://localhost:3001/cards/${id}`, {
+                method: "DELETE"
+            })
+            if (response.status === 200) window.alert("Cartão deletado com sucesso!")
+            location.reload()
+        }
+        return
+    }
 
     return (
         <div className="bg-white rounded-lg p-4 shadow-lg mt-10">
@@ -65,25 +87,47 @@ export default function Payment() {
                                 <h2>+ Adicionar cartão</h2>
                             </button>
                         </Link>
-                        <div className="grid grid-cols-2">
+                        <div className="grid">
                             {cards.map((card:Card) => (
-                                <button
-                                key={card.id}
-                                type="button"
-                                onClick={() => setCardSelected(card.id)}
-                                className={`h-32 bg-white shadow-md px-4 rounded flex justify-between items-center mx-4 mb-4 ${cardSelected === card.id ? "bg-gray-400" : ""}`}
-                                >
-                                    {card.card_type === "MasterCard" ? (<MasterCard/>) : (<Visa/>)}
-                                    <h3>{card.nickname} - {card.code_last4}</h3>
-                                    <Image src={configuracoes} alt="Configurações e mais" className='w-10'/>
-                                </button>
+                                <div key={card.id} className="grid">
+                                    <button
+                                    type="button"
+                                    onClick={() => {setCardSelected(card.id)}}
+                                    className={`h-32 shadow-md px-4 rounded flex justify-between items-center mx-4 mb-4 ${cardSelected === card.id ? "bg-gray-300" : "bg-white"}`}
+                                    >
+                                        {card.card_type === "MasterCard" ? (<MasterCard/>) : (<Visa/>)}
+                                        <h3>{card.nickname} - {card.code_last4}</h3>
+                                        <span onClick={() => atualizarMenu(card.id)} onKeyDown={(e) => { if (e.key === 'Enter') atualizarMenu(card.id); }} className='border-4 rounded-lg p-2'>
+                                            <Image src={configuracoes} alt="Configurações e mais" className='w-10'/>
+                                        </span>
+                                    </button>
+                                    {card.id === menuOpen && (
+                                        <div className="absolute right-0 mt-2 w-32 bg-white shadow-lg rounded-lg overflow-hidden">
+                                            <button
+                                            type='button'
+                                            className="w-full text-left px-4 py-2 hover:bg-gray-100"
+                                            onClick={() => confirmarRemocao(card.id)}
+                                            >
+                                                Remover
+                                            </button>
+                                            <Link href={`/payment/edit_card/${card.id}`}>
+                                                <button
+                                                type='button'
+                                                className="w-full text-left px-4 py-2 hover:bg-gray-100"
+                                                >
+                                                    Atualizar
+                                                </button>
+                                            </Link>
+                                        </div>
+                                    )}
+                                </div>
                             ))}
                         </div>
                     </div>
                 )}
 
                 <div className="text-right align-bottom mt-4">
-                    <button type="submit" className="rounded-lg p-4 shadow-md bg-red-500 text-white">
+                    <button type="button" className="rounded-lg p-4 shadow-md bg-red-500 text-white" onClick={() => history.back()}>
                         <h2>Cancelar</h2>
                     </button>
                     <button type="submit" className="rounded-lg p-4 shadow-md bg-blue-950 text-white ml-4 mr-2">
