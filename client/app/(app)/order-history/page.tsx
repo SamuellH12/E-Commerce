503354ac77ee5ee78854ea9ae5fc959425920357
Samuel Celso dos Anjos Neto <102682697+scan4364@users.noreply.github.com>
2025-02-26 09:22:09 -0300

    fix: frontend POS e OS

--- client/app/(app)/order-history/page.tsx
+++ client/app/(app)/order-history/page.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import { getOrderItemsToOrder, getOrders } from "./../api/order-api";
+import { getOrders, getOrderItemsToOrder } from "./../api/order-api";
 import OrderCard from "./components/order-card";
 
 const OrderHistoryPage = () => {
@@ -35,6 +35,7 @@ const OrderHistoryPage = () => {
       setError("Error loading order details.");
     }
   };
+  
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error}</p>;
@@ -62,11 +63,7 @@ const OrderHistoryPage = () => {
         <h1 className="text-2xl font-bold text-center mb-4">Order History</h1>
         <h2 className="text-xl font-semibold text-center mb-2">Last Orders</h2>
         {recentOrders.map((order) => (
-          <OrderCard
-            key={order.order_id}
-            order={order}
-            onViewDetails={handleViewOrderDetails}
-          />
+          <OrderCard key={order.order_id} order={order} onViewDetails={handleViewOrderDetails} />
         ))}
         <button
           className="mt-4 px-6 py-3 text-lg font-medium text-white bg-green-600 rounded hover:bg-green-700 transition duration-300 block mx-auto w-full max-w-[200px]"
--- client/app/(app)/product-order-history/components/product-order-card.tsx
+++ client/app/(app)/product-order-history/components/product-order-card.tsx
@@ -1,46 +1,4 @@
 // Arquivo: /product-order-history/components/product-order-card.tsx
-/*
-v1
-"use client";
-import React from "react";
-
-interface ProductOrderCardProps {
-  id: string;
-  createdAt: string;
-  productId: string;
-  pricePaid: string;
-  amount: number;
-}
-
-const ProductOrderCard: React.FC<ProductOrderCardProps> = ({
-  id,
-  createdAt,
-  productId,
-  pricePaid,
-  amount,
-}) => {
-  return (
-    <div style={{
-      background: "#fff",
-      border: "1px solid #ddd",
-      padding: "16px",
-      margin: "8px 0",
-      borderRadius: "12px",
-      boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-      transition: "0.3s",
-      cursor: "pointer"
-    }}>
-      <h3 style={{ marginBottom: "8px", color: "#333" }}>Item ID: {id}</h3>
-      <p style={{ color: "#666", fontSize: "14px" }}>Created Date: {createdAt}</p>
-      <p style={{ color: "#666", fontSize: "14px" }}>Product ID: {productId}</p>
-      <p style={{ color: "#666", fontSize: "14px" }}>Price Paid: R$ {pricePaid}</p>
-      <p style={{ color: "#666", fontSize: "14px" }}>Amount: {amount}</p>
-    </div>
-  );
-};
-
-export default ProductOrderCard;*/
-
 "use client";
 import React from "react";
 
