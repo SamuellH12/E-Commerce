0931d4a2671cfcee16395b49f2c9e51060814021
Pedro Baptista <pedrolk2012@gmail.com>
2025-03-20 08:27:12 -0300

    feat(admin): add Breadcrumb to categories and departments

--- client/app/admin/categories/page.tsx
+++ client/app/admin/categories/page.tsx
@@ -1,6 +1,25 @@
 "use client";
-import { axiosApi } from "@/lib/axios-client";
-import { useQuery } from "@tanstack/react-query";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Separator } from "@/components/ui/separator";
 import {
   Sheet,
   SheetClose,
@@ -10,21 +29,11 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet"
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/sheet";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { axiosApi } from "@/lib/axios-client";
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
-
 
 interface CategoryType {
   id: number;
@@ -40,23 +49,20 @@ interface DepartmentType {
 }
 
 export default function Categories() {
-
-
   const [formCat, setFormCat] = useState({
     name: "",
     department_id: "",
   });
   const [catId, setCatId] = useState(-1);
-  
 
-  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setFormCat({...formCat, [e.target.id]: e.target.value });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormCat({ ...formCat, [e.target.id]: e.target.value });
   };
 
-  const handleDepChange = (value : string) => {
-    setFormCat({...formCat, department_id: value});
+  const handleDepChange = (value: string) => {
+    setFormCat({ ...formCat, department_id: value });
   };
-  
+
   const { data, isLoading } = useQuery({
     queryKey: ["categoriesQuery"],
     queryFn: async (): Promise<CategoryType[]> => {
@@ -73,10 +79,10 @@ export default function Categories() {
     },
   });
 
-  async function updateCat(){
-    console.log(JSON.stringify(formCat))
-    console.log(catId)
-    
+  async function updateCat() {
+    console.log(JSON.stringify(formCat));
+    console.log(catId);
+
     const response = await axiosApi(`/category/${catId}`, {
       method: "PUT",
       headers: {
@@ -85,40 +91,40 @@ export default function Categories() {
       data: JSON.stringify(formCat),
     });
 
-    console.log(response.status)
-    
+    console.log(response.status);
+
     if (response.status === 200) {
       window.alert("Categoria atualizada com sucesso!");
       location.reload();
-    };
+    }
+  }
 
-  };
+  async function createCat() {
+    console.log(JSON.stringify(formCat));
 
-  async function createCat(){
-    console.log(JSON.stringify(formCat))
-    
     const response = await axiosApi(`/category/`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       data: JSON.stringify(formCat),
-    }).catch(function(error) {
-      window.alert("Erro ao criar categoria! Verifique se você preencheu todos os campos");
+    }).catch(function (error) {
+      window.alert(
+        "Erro ao criar categoria! Verifique se você preencheu todos os campos"
+      );
     });
 
-    if(!response) return;
-    
+    if (!response) return;
+
     if (response.status === 200) {
       window.alert("Categoria criada com sucesso!");
       location.reload();
     }
-  };
+  }
 
-  async function deleteCat(){
-    if(!confirm('Você tem certeza que quer deletar essa categoria ?'))
-        return;
-    
+  async function deleteCat() {
+    if (!confirm("Você tem certeza que quer deletar essa categoria ?")) return;
+
     const response = await axiosApi(`/category/${catId}`, {
       method: "DELETE",
       headers: {
@@ -129,121 +135,187 @@ export default function Categories() {
     if (response.status === 200) {
       location.reload();
       window.alert("Categoria excluida sucesso!");
-    };
-  };
-
+    }
+  }
 
   if (isLoading || data_dep.isLoading) return null;
 
   console.log("data", data);
   return (
-    <main className="m-auto mt-9 flex flex-col gap-6 pb-20 items-center">
-      <h2 className="text-3xl font-bold"> Categorias  </h2>
-      <div className=" flex justify-center items-center w-full gap-5">
-          
-        { data?.map((cat) => (
-           <Sheet key={cat.id}>
-           <SheetTrigger asChild>
-             <Button variant="outline" onClick={() => setCatId(cat.id)}> {cat.name}</Button>
-           </SheetTrigger>
-           
-           <SheetContent side="bottom">  
-             <SheetHeader>
-               <SheetTitle>Editar Categoria</SheetTitle>
-               <SheetDescription>
-                 Modifique a categoria aqui
-               </SheetDescription>
-             </SheetHeader>
+    <>
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="/admin">Admin</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Categorias</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <main className="m-auto mt-9 flex flex-col gap-6 pb-20 items-center">
+        <h2 className="text-3xl font-bold"> Categorias </h2>
+        <div className=" flex justify-center items-center w-full gap-5">
+          {data?.map((cat) => (
+            <Sheet key={cat.id}>
+              <SheetTrigger asChild>
+                <Button variant="outline" onClick={() => setCatId(cat.id)}>
+                  {" "}
+                  {cat.name}
+                </Button>
+              </SheetTrigger>
 
-             <div className="grid gap-4 py-4">
-               
-               <div className="grid grid-cols-4 items-center gap-4">          
-                 <Label htmlFor="name" className="text-right"> Nome </Label>
-                 <Input id="name" defaultValue={cat.name} className="col-span-3" onChange={handleChange} />
-               </div>
+              <SheetContent side="bottom">
+                <SheetHeader>
+                  <SheetTitle>Editar Categoria</SheetTitle>
+                  <SheetDescription>
+                    Modifique a categoria aqui
+                  </SheetDescription>
+                </SheetHeader>
 
-               <div className="grid grid-cols-4 items-center gap-4">
-                 <Label htmlFor="username" className="text-right"> Departamento </Label>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      {" "}
+                      Nome{" "}
+                    </Label>
+                    <Input
+                      id="name"
+                      defaultValue={cat.name}
+                      className="col-span-3"
+                      onChange={handleChange}
+                    />
+                  </div>
 
-                 <Select defaultValue={""+cat.department_id} onValueChange={handleDepChange}>
-                  <SelectTrigger className="w-[180px]"> <SelectValue placeholder="Selecione o Departamento" /> </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Departamento</SelectLabel>
-                      
-                      { data_dep.data?.map((dep) => (
-                        <SelectItem id={""+dep.id} value={""+dep.id}  key={""+dep.id} > {dep.name} </SelectItem>  
-                      ))}
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="username" className="text-right">
+                      {" "}
+                      Departamento{" "}
+                    </Label>
 
-                    </SelectGroup>
-                  </SelectContent>
-                </Select>
-               
-               </div>
-             </div>
+                    <Select
+                      defaultValue={"" + cat.department_id}
+                      onValueChange={handleDepChange}
+                    >
+                      <SelectTrigger className="w-[180px]">
+                        {" "}
+                        <SelectValue placeholder="Selecione o Departamento" />{" "}
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectGroup>
+                          <SelectLabel>Departamento</SelectLabel>
 
-             <SheetFooter>
-               <SheetClose asChild>
-                 <Button type="button" onClick={updateCat}>Save changes</Button>
-               </SheetClose>
-               <SheetClose asChild>
-                 <Button type="button" variant="destructive" onClick={deleteCat}>Delete</Button>
-               </SheetClose>
-             </SheetFooter>
-           </SheetContent>
-         </Sheet>
-        ))}
+                          {data_dep.data?.map((dep) => (
+                            <SelectItem
+                              id={"" + dep.id}
+                              value={"" + dep.id}
+                              key={"" + dep.id}
+                            >
+                              {" "}
+                              {dep.name}{" "}
+                            </SelectItem>
+                          ))}
+                        </SelectGroup>
+                      </SelectContent>
+                    </Select>
+                  </div>
+                </div>
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline"> + </Button>
-          </SheetTrigger>
-          
-          <SheetContent side="top">  
-            <SheetHeader>
-              <SheetTitle>Criar Categoria</SheetTitle>
-              <SheetDescription>
-                Coloque as informações da categoria
-              </SheetDescription>
-            </SheetHeader>
+                <SheetFooter>
+                  <SheetClose asChild>
+                    <Button type="button" onClick={updateCat}>
+                      Save changes
+                    </Button>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Button
+                      type="button"
+                      variant="destructive"
+                      onClick={deleteCat}
+                    >
+                      Delete
+                    </Button>
+                  </SheetClose>
+                </SheetFooter>
+              </SheetContent>
+            </Sheet>
+          ))}
 
-            <div className="grid gap-4 py-4">
-              
-              <div className="grid grid-cols-4 items-center gap-4">          
-                <Label htmlFor="name" className="text-right"> Nome </Label>
-                <Input id="name" className="col-span-3" onChange={handleChange} />
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline"> + </Button>
+            </SheetTrigger>
+
+            <SheetContent side="top">
+              <SheetHeader>
+                <SheetTitle>Criar Categoria</SheetTitle>
+                <SheetDescription>
+                  Coloque as informações da categoria
+                </SheetDescription>
+              </SheetHeader>
+
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    {" "}
+                    Nome{" "}
+                  </Label>
+                  <Input
+                    id="name"
+                    className="col-span-3"
+                    onChange={handleChange}
+                  />
+                </div>
+
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="username" className="text-right">
+                    {" "}
+                    Departamento{" "}
+                  </Label>
+
+                  <Select onValueChange={handleDepChange}>
+                    <SelectTrigger className="w-[180px]">
+                      {" "}
+                      <SelectValue placeholder="Selecione o Departamento" />{" "}
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Departamento</SelectLabel>
+
+                        {data_dep.data?.map((dep) => (
+                          <SelectItem
+                            id={"" + dep.id}
+                            value={"" + dep.id}
+                            key={"" + dep.id}
+                          >
+                            {" "}
+                            {dep.name}{" "}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
               </div>
 
-              <div className="grid grid-cols-4 items-center gap-4">
-                <Label htmlFor="username" className="text-right"> Departamento </Label>
-
-                <Select onValueChange={handleDepChange}>
-                <SelectTrigger className="w-[180px]"> <SelectValue placeholder="Selecione o Departamento" /> </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Departamento</SelectLabel>
-                    
-                    { data_dep.data?.map((dep) => (
-                      <SelectItem id={""+dep.id} value={""+dep.id}  key={""+dep.id} > {dep.name} </SelectItem>  
-                    ))}
-
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-              
-              </div>
-            </div>
-
-            <SheetFooter>
-              <SheetClose asChild>
-                <Button type="button" onClick={createCat}>Save</Button>
-              </SheetClose>
-              
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
-
-      </div>  
-    </main>
+              <SheetFooter>
+                <SheetClose asChild>
+                  <Button type="button" onClick={createCat}>
+                    Save
+                  </Button>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </main>
+    </>
   );
 }
--- client/app/admin/departments/page.tsx
+++ client/app/admin/departments/page.tsx
@@ -1,6 +1,16 @@
 "use client";
-import { axiosApi } from "@/lib/axios-client";
-import { useQuery } from "@tanstack/react-query";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator";
 import {
   Sheet,
   SheetClose,
@@ -10,19 +20,10 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from "@/components/ui/sheet"
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/sheet";
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { axiosApi } from "@/lib/axios-client";
+import { useQuery } from "@tanstack/react-query";
 import React, { useState } from "react";
 
 interface DepartmentType {
@@ -32,14 +33,13 @@ interface DepartmentType {
 }
 
 export default function Departments() {
-
   const [formDep, setFormDep] = useState({
-    name: "", 
+    name: "",
   });
   const [depId, setDepId] = useState(-1);
 
-  const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
-    setFormDep({...formDep, [e.target.id]: e.target.value });
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormDep({ ...formDep, [e.target.id]: e.target.value });
   };
   const { data, isLoading } = useQuery({
     queryKey: ["categoriesQuery"],
@@ -49,10 +49,10 @@ export default function Departments() {
     },
   });
 
-  async function updateDep(){
-    console.log(JSON.stringify(formDep))
-    console.log(depId)
-    
+  async function updateDep() {
+    console.log(JSON.stringify(formDep));
+    console.log(depId);
+
     const response = await axiosApi(`/department/${depId}`, {
       method: "PUT",
       headers: {
@@ -61,39 +61,40 @@ export default function Departments() {
       data: JSON.stringify(formDep),
     });
 
-    console.log(response.status)
-    
+    console.log(response.status);
+
     if (response.status === 200) {
       location.reload();
       window.alert("Departamento atualizada com sucesso!");
-    };
-  };
+    }
+  }
 
-  async function createDep(){
-    console.log(JSON.stringify(formDep))
-    
+  async function createDep() {
+    console.log(JSON.stringify(formDep));
+
     const response = await axiosApi(`/department/`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       data: JSON.stringify(formDep),
-    }).catch(function(error) {
-      window.alert("Erro ao criar departamento! Verifique se o nome inserido é válido.");
+    }).catch(function (error) {
+      window.alert(
+        "Erro ao criar departamento! Verifique se o nome inserido é válido."
+      );
     });
 
-    if(!response) return;
-    
+    if (!response) return;
+
     if (response.status === 200) {
       window.alert("Departamento criado com sucesso!");
       location.reload();
     }
-  };
+  }
 
-  async function deleteDep(){
-    if(!confirm('Você tem certeza que quer deletar essa categoria ?'))
-        return;
-    
+  async function deleteDep() {
+    if (!confirm("Você tem certeza que quer deletar essa categoria ?")) return;
+
     const response = await axiosApi(`/department/${depId}`, {
       method: "DELETE",
       headers: {
@@ -104,85 +105,121 @@ export default function Departments() {
     if (response.status === 200) {
       location.reload();
       window.alert("Departamento excluido sucesso!");
-    };
-  };
-
+    }
+  }
 
   if (isLoading) return null;
 
-  
   return (
-    <main className="m-auto mt-9 flex flex-col gap-6 pb-20 items-center">
-      <h2 className="text-3xl font-bold"> Departamentos  </h2>
-      <div className=" flex justify-center items-center w-full gap-5">
-          
-        { data?.map((dep) => (
-           <Sheet key={dep.id}>
-           <SheetTrigger asChild>
-             <Button variant="outline" onClick={() => setDepId(dep.id)}> {dep.name} </Button>
-           </SheetTrigger>
-           
-           <SheetContent side="bottom">  
-             <SheetHeader>
-               <SheetTitle>Editar Departamento</SheetTitle>
-               <SheetDescription>
-                 Modifique o Departamento
-               </SheetDescription>
-             </SheetHeader>
+    <>
+      <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <div className="flex items-center gap-2 px-4">
+          <SidebarTrigger className="-ml-1" />
+          <Separator orientation="vertical" className="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem className="hidden md:block">
+                <BreadcrumbLink href="/admin">Admin</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator className="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Departamentos</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+      <main className="m-auto mt-9 flex flex-col gap-6 pb-20 items-center">
+        <h2 className="text-3xl font-bold"> Departamentos </h2>
+        <div className=" flex justify-center items-center w-full gap-5">
+          {data?.map((dep) => (
+            <Sheet key={dep.id}>
+              <SheetTrigger asChild>
+                <Button variant="outline" onClick={() => setDepId(dep.id)}>
+                  {" "}
+                  {dep.name}{" "}
+                </Button>
+              </SheetTrigger>
 
-             <div className="grid gap-4 py-4">
-               
-               <div className="grid grid-cols-4 items-center gap-4">          
-                 <Label htmlFor="name" className="text-right"> Nome </Label>
-                 <Input id="name" defaultValue={dep.name} className="col-span-3" onChange={handleChange} />
-               </div>
+              <SheetContent side="bottom">
+                <SheetHeader>
+                  <SheetTitle>Editar Departamento</SheetTitle>
+                  <SheetDescription>Modifique o Departamento</SheetDescription>
+                </SheetHeader>
 
-             </div>
+                <div className="grid gap-4 py-4">
+                  <div className="grid grid-cols-4 items-center gap-4">
+                    <Label htmlFor="name" className="text-right">
+                      {" "}
+                      Nome{" "}
+                    </Label>
+                    <Input
+                      id="name"
+                      defaultValue={dep.name}
+                      className="col-span-3"
+                      onChange={handleChange}
+                    />
+                  </div>
+                </div>
 
-             <SheetFooter>
-               <SheetClose asChild>
-                 <Button type="button" onClick={updateDep}>Save changes</Button>
-               </SheetClose>
-               <SheetClose asChild>
-                 <Button type="button" variant="destructive" onClick={deleteDep}>Delete</Button>
-               </SheetClose>
-             </SheetFooter>
-           </SheetContent>
-         </Sheet>
-        ))}
+                <SheetFooter>
+                  <SheetClose asChild>
+                    <Button type="button" onClick={updateDep}>
+                      Save changes
+                    </Button>
+                  </SheetClose>
+                  <SheetClose asChild>
+                    <Button
+                      type="button"
+                      variant="destructive"
+                      onClick={deleteDep}
+                    >
+                      Delete
+                    </Button>
+                  </SheetClose>
+                </SheetFooter>
+              </SheetContent>
+            </Sheet>
+          ))}
 
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline"> + </Button>
-          </SheetTrigger>
-          
-          <SheetContent side="top">  
-            <SheetHeader>
-              <SheetTitle>Criar Categoria</SheetTitle>
-              <SheetDescription>
-                Coloque as informações da categoria
-              </SheetDescription>
-            </SheetHeader>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline"> + </Button>
+            </SheetTrigger>
 
-            <div className="grid gap-4 py-4">
-              
-              <div className="grid grid-cols-4 items-center gap-4">          
-                <Label htmlFor="name" className="text-right"> Nome </Label>
-                <Input id="name" className="col-span-3" onChange={handleChange} />
+            <SheetContent side="top">
+              <SheetHeader>
+                <SheetTitle>Criar Categoria</SheetTitle>
+                <SheetDescription>
+                  Coloque as informações da categoria
+                </SheetDescription>
+              </SheetHeader>
+
+              <div className="grid gap-4 py-4">
+                <div className="grid grid-cols-4 items-center gap-4">
+                  <Label htmlFor="name" className="text-right">
+                    {" "}
+                    Nome{" "}
+                  </Label>
+                  <Input
+                    id="name"
+                    className="col-span-3"
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
 
-            </div>
-
-            <SheetFooter>
-              <SheetClose asChild>
-                <Button type="button" onClick={createDep}>Save</Button>
-              </SheetClose>
-              
-            </SheetFooter>
-          </SheetContent>
-        </Sheet>
-
-      </div>  
-    </main>
+              <SheetFooter>
+                <SheetClose asChild>
+                  <Button type="button" onClick={createDep}>
+                    Save
+                  </Button>
+                </SheetClose>
+              </SheetFooter>
+            </SheetContent>
+          </Sheet>
+        </div>
+      </main>
+    </>
   );
 }
