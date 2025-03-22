909b32b44ec1f83970ead0b3582a0f3ffde1778d
Pedro Baptista <pedrolk2012@gmail.com>
2025-03-22 20:52:44 -0300

    test: create cypress and cucumber tests integration

--- client/cypress/support/e2e.ts
+++ client/cypress/support/e2e.ts
@@ -14,4 +14,41 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
+import 'cypress-file-upload';
+
+declare global {
+  namespace Cypress {
+    interface Chainable<Subject = any> {
+      /**
+       * Custom command to select DOM element by data-cy attribute.
+       * @example cy.dataCy('greeting')
+       */
+      dataCy(value: string): Chainable<JQuery<HTMLElement>>;
+      /**
+       * Custom command to upload a file
+       * @example cy.get('[data-cy=upload]').attachFile('example.json')
+       */
+      attachFile(fileName: string): Chainable<JQuery<HTMLElement>>;
+    }
+  }
+}
+
+// Add custom command for selecting elements by data-cy attribute
+Cypress.Commands.add('dataCy', (value: string): Cypress.Chainable<JQuery<HTMLElement>> => {
+  return cy.get(`[data-cy=${value}]`);
+});
+Cypress.on("uncaught:exception", (err) => {
+  // Cypress and React Hydrating the document don't get along
+  // for some unknown reason. Hopefully, we figure out why eventually
+  // so we can remove this.
+  // https://github.com/remix-run/remix/issues/4822#issuecomment-1679195650
+  // https://github.com/cypress-io/cypress/issues/27204
+  if (
+    /hydrat/i.test(err.message) ||
+    /Minified React error #418/.test(err.message) ||
+    /Minified React error #423/.test(err.message)
+  ) {
+    return false
+  }
+});
